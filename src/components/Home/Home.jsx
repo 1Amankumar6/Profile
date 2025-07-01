92fcb7d899299f6ca70dd0823d98b0a23e0b1ac4
Aman Kumar <98329171+1Amankumar6@users.noreply.github.com>
2025-07-02 00:23:33 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -68,7 +68,7 @@ const Home = () => {
               className="text-xl bg-[#00adb5] text-[#eeeeee] px-4 py-2 rounded-full shadow-lg hover:bg-[#007880]/90"
               style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.6)" }}
             >
-              <Link to="/contact">Hire me</Link>
+              <a href="#contact">Hire me</a>
             </button>
             <button
               onClick={() => useResume()}
